3075bf9879557db4f253ca1ce7ab2b3bd1de60c6
jimmy-bellinghausen <jimmy.bellinghausen@gmail.com>
2020-02-25 23:18:36 -0600

    Completed assignment

--- src/main/java/com/galvanize/cart/Cart.java
+++ src/main/java/com/galvanize/cart/Cart.java
@@ -1,4 +1,112 @@
 package com.galvanize.cart;
 
+import java.util.ArrayList;
+import java.util.TreeMap;
+import java.util.Map;
+
 public class Cart {
+/*
+*Mutable type that represents a shopping cart
+*
+* Abstraction Function:
+*       AF(items,cachedTotal) = shopping cart with a list of items and a total cost of what's inside
+*
+* Not Threadsafe
+ */
+    private ArrayList<Item> items;
+    private double cachedTotal;
+
+    public Cart(){
+    /*
+    * creates an empty cart
+     */
+        items = new ArrayList<Item>();
+        cachedTotal=0;
+    }
+
+    public ArrayList<String> ItemQuantities(){
+    /*
+    * returns list of item quantities in shopping cart
+     */
+        TreeMap<String,Integer> tracker = new TreeMap<String, Integer>();
+        for(Item item : items){
+            tracker.compute(item.getName(), (key,value) -> (value==null) ? 1 : value + 1 );
+        }
+        ArrayList<String> quantitativeList = new ArrayList<String>();
+        for(Map.Entry<String, Integer> item : tracker.entrySet()){
+            quantitativeList.add(item.getKey()+" - "+item.getValue());
+        }
+        return quantitativeList;
+    }
+
+    public ArrayList<String> ItemizedList(){
+    /*
+     * returns itemized list of items in shopping cart
+     */
+        TreeMap<String,Double> tracker = new TreeMap<String, Double>();
+        for(Item item : items){
+            tracker.put(item.getName(),item.getCost());
+        }
+        ArrayList<String> itemizedList = new ArrayList<String>();
+        for(Map.Entry<String, Double> item : tracker.entrySet()){
+            itemizedList.add(item.getKey()+" - $"+item.getValue());
+        }
+        return itemizedList;
+    }
+
+    public ArrayList<String> OnSaleItems(){
+    /*
+     * returns list of items on sale in shopping cart
+     */
+        TreeMap<String,Double> tracker = new TreeMap<String, Double>();
+        for(Item item : items){
+            if(item.getSaleStatus()) {
+                tracker.put(item.getName(), item.getCost());
+            }
+        }
+        ArrayList<String> itemizedList = new ArrayList<String>();
+        for(Map.Entry<String, Double> item : tracker.entrySet()){
+            itemizedList.add(item.getKey()+" - $"+item.getValue());
+        }
+        return itemizedList;
+    }
+
+    public void AddItem(Item item){
+    /*
+    * Adds item to shopping cart
+    *
+    * @params
+    *       item - Item to be added
+     */
+        this.items.add(item);
+        this.cachedTotal+=item.getCost();
+    }
+
+    public void AddItem(Item item, int quantity){
+    /*
+     * Adds specified quantity of items to shopping cart
+     *
+     * @params
+     *      item - Item to be added
+     *      quantity - how many items to be added
+     */
+        for(int x=0;x<quantity;x++){
+            this.items.add(item);
+        }
+        this.cachedTotal+=(item.getCost()*quantity);
+    }
+
+    public ArrayList<Item> getItems(){
+    /*
+    * returns list of items in shopping cart
+     */
+        return this.items;
+    }
+
+    public double getTotalPrice(){
+    /*
+    * returns total price of items in cart
+     */
+        return this.cachedTotal;
+    }
 }
--- src/main/java/com/galvanize/cart/Item.java
+++ src/main/java/com/galvanize/cart/Item.java
@@ -1,4 +1,66 @@
 package com.galvanize.cart;
 
 public class Item {
+/*
+*Immutable type representing a store item
+*
+* Rep Invariant:
+*       cost > 0
+* Abstraction Function:
+*       AF(name,cost,onSale) = an item with a name, cost, sale status.
+* Thread Safety Argument:
+*       This class is threadsafe because it's immutable:
+*           - all properties are encapsulated in this object and never changed after construction.
+ */
+
+    private String name;
+    private double cost;
+    private boolean onSale=false;
+
+    public Item(String name, double cost){
+    /*
+    *creates an item with specified name and cost and is not on sale.
+    *
+    *@params
+    *   name - name to be displayed by list
+    *   cost - cost in  currency
+    */
+        this.name=name;
+        this.cost=cost;
+    }
+
+
+    public Item(String name, double cost, boolean onSale){
+    /*
+     *creates an item with specified name, cost and sale status.
+     *
+     *@params
+     *   name - name to be displayed by list
+     *   cost - cost in currency
+     *   onSale - indicates if the item is on sale
+     */
+        this(name,cost);
+        this.onSale=onSale;
+    }
+
+    public double getCost(){
+    /*
+    * returns item cost
+     */
+        return this.cost;
+    }
+
+    public String getName(){
+    /*
+    * returns item name
+     */
+        return this.name;
+    }
+
+    public boolean getSaleStatus(){
+    /*
+    * returns item sale status
+     */
+        return this.onSale;
+    }
 }
